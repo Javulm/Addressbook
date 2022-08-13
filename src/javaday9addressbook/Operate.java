86f0bd73c43da7a44960fd53216136772baa987d
Javul Mulla <javulmulla8@gmail.com>
2022-08-13 17:00:05 +0530

    Added: delete method using first name.

--- src/javaday9addressbook/Operate.java
+++ src/javaday9addressbook/Operate.java
@@ -3,32 +3,47 @@ package javaday9addressbook;
 import java.util.Scanner;
 
 class Operate {
-    public void editContact(AddressBook contact) {
+	public void editContact(AddressBook contact) {
 
-        String address;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("\nEnter the First name for update Address : ");
-        String editName = sc.nextLine();
-        if (editName.equals(contact.getFirstName())) {
-            System.out.print("\nEnter your new Address : ");
-            address = sc.nextLine();
-            contact.setAddress(address);
-        } else {
-            System.out.print("\ncouldn't find the contact..");
-        }
-        sc.close();
-    }
+		String address;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter the First name for update Address : ");
+		String editName = sc.nextLine();
+		if (editName.equals(contact.getFirstName())) {
+			System.out.print("\nEnter your new Address : ");
+			address = sc.nextLine();
+			contact.setAddress(address);
+		} else {
+			System.out.print("\ncouldn't find the contact..");
+		}
+		sc.close();
+	}
+	public AddressBook deleteContact(AddressBook contact) {
 
-    public void showContact(AddressBook contact) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("\nEnter the First name for delete : ");
+		String deleteName = sc.nextLine();
+		if (deleteName.equals(contact.getFirstName())) {
+			AddressBook detail = new AddressBook();
+			System.out.print("\nYour contact deleted..");
+			return detail;
+		} else {
+			System.out.print("\ncouldn't find the contact..");
+		}
+		sc.close();
+		return contact;
+	}
 
-        System.out.print("\n-----------------");
-        System.out.print("\nFirst Name  : " + contact.getFirstName());
-        System.out.print("\nLast Name   : " + contact.getLastName());
-        System.out.print("\nAddress     : " + contact.getAddress());
-        System.out.print("\nCity        : " + contact.getCity());
-        System.out.print("\nState       : " + contact.getState());
-        System.out.print("\nPone Number : " + contact.getPhoneNo());
-        System.out.print("\nE-mail      : " + contact.getEmail());
-        System.out.print("\nZip         : " + contact.getZip());
-    }
+	public void showContact(AddressBook contact) {
+
+		System.out.print("\n-----------------");
+		System.out.print("\nFirst Name  : " + contact.getFirstName());
+		System.out.print("\nLast Name   : " + contact.getLastName());
+		System.out.print("\nAddress     : " + contact.getAddress());
+		System.out.print("\nCity        : " + contact.getCity());
+		System.out.print("\nState       : " + contact.getState());
+		System.out.print("\nPone Number : " + contact.getPhoneNo());
+		System.out.print("\nE-mail      : " + contact.getEmail());
+		System.out.print("\nZip         : " + contact.getZip());
+	}
 }
